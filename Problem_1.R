541799fb40ea73f7e23d8056cb500cc9e03e3157
Viktoria Gasparova <vgasparova96@gmail.com>
2018-04-02 17:48:12 +0300

    .

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,28 @@
+#a
+x <- c(4,1,1,4)
+
+#b
+y <- c(1,4)
+
+#c
+x-y
+#subtraction is performed elementwise and if two vectors are of unequal length, 
+#the shorter one will be recycled in order to match the longer vector. 
+
+#d 
+s <- c(x,y)
+
+#e
+rep(s, 10)
+length(rep(s, 10))
+
+#f
+rep(s, each=3)
+
+#g
+seq.default(from=7, to=21, by=1)
+7:21
+
+#h
+length(7:21)
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,58 @@
+#a
+xmin <- c(23,20.5,28.2,20.3,22.4,17.2,18.2)
+xmax <- c(25.0,22.8,31.2,27.3,28.4,20.2,24.1)
+
+#b
+xmax-xmin
+
+#c
+totalxmin <- sum(xmin)
+totalxmin
+avgxmin <- totalxmin/length(xmin)
+avgxmin
+
+avgxmax <- mean(xmax)
+avgxmax
+
+#d
+xmin<avgxmin
+xmin[xmin<avgxmin]
+
+#e
+xmin>avgxmin
+xmin[xmin>avgxmin]
+
+#f
+dayNames <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+names(xmin) <- dayNames
+str(xmin)
+names(xmax) <- dayNames
+str(xmax)
+
+#g
+temperatures <- data.frame(
+  df.xmin = xmin, 
+  df.xmax = xmax)
+dim(temperatures)
+temperatures
+
+#h
+xminFahrenheit <- c(((9/5)*xmin)+32) #Fahrenheit
+xminFahrenheit
+cbind(temperatures, xminFahrenheit) -> temperatures
+temperatures
+
+#i
+fahrenheit <- data.frame (df.xminFahrenheit = xminFahrenheit)
+dim(fahrenheit)
+fahrenheit
+
+#j
+MonFriFahrenheit <- xminFahrenheit[1:5]
+fivefahrenheit <- data.frame (df.MonFriFahrenheit = MonFriFahrenheit)
+fivefahrenheit
+
+MonFriFahrenheit2 <- xminFahrenheit[-c(6,7)]
+fivefahrenheit2 <- data.frame (df.MonFriFahrenheit2 = MonFriFahrenheit2)
+fivefahrenheit2
 
