1511c72e5a32ca908273ca0754b464f0cf5bdced
Boyko Amarov <boyko.amarov@gmail.com>
2018-04-05 13:01:35 +0300

    Add review.

--- Problem_2.R
+++ Problem_2.R
@@ -6,6 +6,7 @@ xmax <- c(25.0,22.8,31.2,27.3,28.4,20.2,24.1)
 xmax-xmin
 
 #c
+## Use the 'mean' function
 totalxmin <- sum(xmin)
 totalxmin
 avgxmin <- totalxmin/length(xmin)
@@ -20,9 +21,15 @@ xmin[xmin<avgxmin]
 
 #e
 xmin>avgxmin
+
+## Incorrect subsetting
 xmin[xmin>avgxmin]
 
+## Correct
+xmin[xmax > avgmax]
+
 #f
+## Nice
 dayNames <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
 names(xmin) <- dayNames
 str(xmin)
@@ -39,6 +46,10 @@ temperatures
 #h
 xminFahrenheit <- c(((9/5)*xmin)+32) #Fahrenheit
 xminFahrenheit
+
+## Hard to read, write it like this
+temperatures <- cbind(temperatures, xminFahrenheit)
+
 cbind(temperatures, xminFahrenheit) -> temperatures
 temperatures
 
@@ -55,4 +66,20 @@ fivefahrenheit
 MonFriFahrenheit2 <- xminFahrenheit[-c(6,7)]
 fivefahrenheit2 <- data.frame (df.MonFriFahrenheit2 = MonFriFahrenheit2)
 fivefahrenheit2
+
+## Easier way to do the above
+
+temperatures <- within(temperatures, {
+  xminFahrenheit <- xmin * (9/5) + 32
+  xmaxFahrenheit <- xmax * (9/5) + 32
+})
+
+temperaturesFahrenheit <- temeratures[, c('xminFahrenheit', 'xmaxFahrenheit)]
+
+## Easier to subset the whole data.frame instead of 
+## doing this for each vector used in its construction
+
+temperaturesFahrenheit[1:5, ]
+temperaturesFahrenheit[-(6:7), ]
+
 
